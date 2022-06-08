d47071548ba3fe909bb6338781b1641ba32666d1
jackie1in <hilin2333@gmail.com>
2022-06-08 15:13:00 +0800

    upgrade springfox

--- mall-portal/src/main/java/com/macro/mall/portal/config/SwaggerConfig.java
+++ mall-portal/src/main/java/com/macro/mall/portal/config/SwaggerConfig.java
@@ -3,14 +3,14 @@ package com.macro.mall.portal.config;
 import com.macro.mall.common.config.BaseSwaggerConfig;
 import com.macro.mall.common.domain.SwaggerProperties;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * Swagger API文档相关配置
  * Created by macro on 2018/4/26.
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig extends BaseSwaggerConfig {
 
     @Override
